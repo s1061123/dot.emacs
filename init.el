5ad7af281dba6a5a446aa29b6eacfa6f0fb1cb2d
Tomofumi Hayashi <tohayash@redhat.com>
2019-03-16 04:25:29 +0900

    mod

--- init.el
+++ init.el
@@ -150,7 +150,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (markdown-mode dockerfile-mode company-box tidal w3m sclang-snippets sclang-extensions howm elscreen ddskk wanderlust go-mode))))
+    (yaml-mode markdown-mode dockerfile-mode company-box tidal w3m sclang-snippets sclang-extensions howm elscreen ddskk wanderlust go-mode))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
